de115dd6d107a4ac6065201a85c771fa37173716
cleavatron <afairiz@gmail.com>
2017-07-16 00:42:02 +0800

    FacialExpression model added

--- FaceIt/ViewController.swift
+++ FaceIt/ViewController.swift
@@ -10,15 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+  @IBOutlet weak var faceView: FaceView!
+  
+  var expression = FacialExpression(eyes: .open, mouth: .grin)
+  
+  
 
 
 }
