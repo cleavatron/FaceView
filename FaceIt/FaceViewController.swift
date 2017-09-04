8902c2c0f2bce0425ea178dc6e48784893a014df
cleavatron <afairiz@gmail.com>
2017-09-04 16:28:57 +0800

    Renamed to FaceViewController

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Ahmad Fairiz on 02/07/2017.
+//  Copyright © 2017 Recite Lab. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+  @IBOutlet weak var faceView: FaceView!{
+    didSet{
+      let handler = #selector(FaceView.changeScale(byReactingTo:))
+      let pinchRecognizer = UIPinchGestureRecognizer(target: faceView, action: handler)
+      faceView.addGestureRecognizer(pinchRecognizer)
+      let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleEyes(byReactingTo:)))
+      tapRecognizer.numberOfTapsRequired = 1
+      faceView.addGestureRecognizer(tapRecognizer)
+      let swipeUpRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(increaseHappiness))
+      swipeUpRecognizer.direction = .up
+      faceView.addGestureRecognizer(swipeUpRecognizer)
+      let swipeDownRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(decreaseHappiness))
+      swipeDownRecognizer.direction = .down
+      faceView.addGestureRecognizer(swipeDownRecognizer)
+      
+      updateUI()
+    }
+  }
+  
+  func toggleEyes(byReactingTo tapRecognizer: UITapGestureRecognizer)
+  {
+    if tapRecognizer.state == .ended{
+      let eyes: FacialExpression.Eyes = (expression.eyes == .closed) ? .open : .closed
+      expression = FacialExpression(eyes: eyes, mouth: expression.mouth)
+    }
+  }
+  
+  func increaseHappiness(){
+    expression = expression.happier
+  }
+  
+  func decreaseHappiness(){
+    expression = expression.sadder
+  }
+  
+  var expression = FacialExpression(eyes: .closed, mouth: .frown){
+    didSet{
+      updateUI()
+    }
+  }
+  
+  private func updateUI()
+  {
+    switch expression.eyes {
+    case .open:
+      faceView?.eyesOpen = true
+    case .closed:
+      faceView?.eyesOpen = false
+    case .squinting:
+      faceView?.eyesOpen = false
+    }
+    faceView?.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+  }
+  
+  private let mouthCurvatures = [FacialExpression.Mouth.grin: 0.5, .frown: -1.0, .smile: 1.0,.neutral: 0.0,.smirk: -0.5]
+
+
+}
+
